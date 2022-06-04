f06c43709b40e0e5152293fda4080f6b583d2513
hsnaimeh <hish.sn.dev@gmail.com>
2022-06-03 22:05:20 -0400

    Finish Assigment 1

---
+++ app/routes/index.server.routes.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+const indexController = require('../controllers/index.server.controller')
+
+/* GET home page. */
+router.get('/', indexController.home);
+
+/* GET Project page. */
+router.get('/projects', indexController.projects);
+
+/* GET about page. */
+router.get('/about', indexController.about);
+
+/* GET services page. */
+router.get('/services', indexController.services);
+
+/* GET contact page. */
+router.get('/contact', indexController.contact);
+
+module.exports = router;
+
