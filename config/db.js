d2f532f4f3cc3461adf84ff481d59603ca165893
snaimeh-alhisba <101411167+snaimeh-alhisba@users.noreply.github.com>
2022-06-18 09:11:25 -0400

    integrate it with mongodb Atlas Cloud

--- config/db.js
+++ config/db.js
@@ -1,3 +1,4 @@
 module.exports = {
-    "URI": "mongodb://localhost/test"
+    // "URI": "mongodb://localhost/test",
+    "URI": "mongodb+srv://Hisham:Wc8RbyjuPwTZgvhu@cluster0.imvf1.mongodb.net/hisham_portfolio?retryWrites=true&w=majority"
 }
